690114bfbae14bf083ecf88f408a4000181e6839
Marcus Vinicius <111466820+MarcusSouza02@users.noreply.github.com>
2025-04-10 19:06:02 -0300

    Delete Relatório 6/src directory

--- Relatório 6/src/carro.rs
+++
@@ -1,3 +0,0 @@
-pub trait Carro {
-    fn descrever(&self);
-}
--- Relatório 6/src/ex1.rs
+++
@@ -1,7 +0,0 @@
-fn main () {
-    let x = 5;
-    let y = 10;
-    let z = 3;
-    let med = (x + y + z)/3;
-    println!("med: {}", med);
-}
